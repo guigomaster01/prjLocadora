f6ba5c0824678e5edb5b59123c942d2fa2009b89
guigomaster01 <contatorodrigorodrigues@gmail.com>
2024-03-11 20:29:59 -0300

    Botao Novo do frmProdutora configurado com o BD da escola

--- prjLocadora/frmProdutoras.Designer.cs
+++ prjLocadora/frmProdutoras.Designer.cs
@@ -51,17 +51,19 @@ namespace prjLocadora
             // 
             // txtCodProd
             // 
-            this.txtCodProd.Location = new System.Drawing.Point(105, 19);
+            this.txtCodProd.Location = new System.Drawing.Point(158, 29);
+            this.txtCodProd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtCodProd.Name = "txtCodProd";
-            this.txtCodProd.Size = new System.Drawing.Size(100, 20);
+            this.txtCodProd.Size = new System.Drawing.Size(148, 26);
             this.txtCodProd.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 22);
+            this.label1.Location = new System.Drawing.Point(15, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.Size = new System.Drawing.Size(133, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Código Produtora";
             // 
@@ -76,138 +78,156 @@ namespace prjLocadora
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.txtCodProd);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(15, 47);
+            this.groupBox1.Location = new System.Drawing.Point(22, 72);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(573, 98);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(860, 151);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Dados da Produtora";
             // 
             // txtEmailProd
             // 
-            this.txtEmailProd.Location = new System.Drawing.Point(270, 48);
+            this.txtEmailProd.Location = new System.Drawing.Point(405, 74);
+            this.txtEmailProd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtEmailProd.Name = "txtEmailProd";
-            this.txtEmailProd.Size = new System.Drawing.Size(271, 20);
+            this.txtEmailProd.Size = new System.Drawing.Size(404, 26);
             this.txtEmailProd.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(215, 51);
+            this.label4.Location = new System.Drawing.Point(322, 78);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(53, 20);
             this.label4.TabIndex = 7;
             this.label4.Text = "E-mail";
             // 
             // txtTelProd
             // 
-            this.txtTelProd.Location = new System.Drawing.Point(105, 45);
+            this.txtTelProd.Location = new System.Drawing.Point(158, 69);
+            this.txtTelProd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtTelProd.Name = "txtTelProd";
-            this.txtTelProd.Size = new System.Drawing.Size(100, 20);
+            this.txtTelProd.Size = new System.Drawing.Size(148, 26);
             this.txtTelProd.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(10, 48);
+            this.label3.Location = new System.Drawing.Point(15, 74);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.Size = new System.Drawing.Size(71, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "Telefone";
             // 
             // txtProd
             // 
-            this.txtProd.Location = new System.Drawing.Point(270, 19);
+            this.txtProd.Location = new System.Drawing.Point(405, 29);
+            this.txtProd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtProd.Name = "txtProd";
-            this.txtProd.Size = new System.Drawing.Size(271, 20);
+            this.txtProd.Size = new System.Drawing.Size(404, 26);
             this.txtProd.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(211, 22);
+            this.label2.Location = new System.Drawing.Point(316, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.Size = new System.Drawing.Size(79, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Produtora";
             // 
             // btnPrimeiro
             // 
-            this.btnPrimeiro.Location = new System.Drawing.Point(145, 151);
+            this.btnPrimeiro.Location = new System.Drawing.Point(218, 232);
+            this.btnPrimeiro.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnPrimeiro.Name = "btnPrimeiro";
-            this.btnPrimeiro.Size = new System.Drawing.Size(75, 23);
+            this.btnPrimeiro.Size = new System.Drawing.Size(112, 35);
             this.btnPrimeiro.TabIndex = 3;
             this.btnPrimeiro.Text = "<<";
             this.btnPrimeiro.UseVisualStyleBackColor = true;
             // 
             // btnAnterior
             // 
-            this.btnAnterior.Location = new System.Drawing.Point(226, 151);
+            this.btnAnterior.Location = new System.Drawing.Point(339, 232);
+            this.btnAnterior.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAnterior.Name = "btnAnterior";
-            this.btnAnterior.Size = new System.Drawing.Size(75, 23);
+            this.btnAnterior.Size = new System.Drawing.Size(112, 35);
             this.btnAnterior.TabIndex = 4;
             this.btnAnterior.Text = "<";
             this.btnAnterior.UseVisualStyleBackColor = true;
             // 
             // btnProximo
             // 
-            this.btnProximo.Location = new System.Drawing.Point(307, 151);
+            this.btnProximo.Location = new System.Drawing.Point(460, 232);
+            this.btnProximo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnProximo.Name = "btnProximo";
-            this.btnProximo.Size = new System.Drawing.Size(75, 23);
+            this.btnProximo.Size = new System.Drawing.Size(112, 35);
             this.btnProximo.TabIndex = 5;
             this.btnProximo.Text = ">";
             this.btnProximo.UseVisualStyleBackColor = true;
             // 
             // btnUltimo
             // 
-            this.btnUltimo.Location = new System.Drawing.Point(388, 151);
+            this.btnUltimo.Location = new System.Drawing.Point(582, 232);
+            this.btnUltimo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnUltimo.Name = "btnUltimo";
-            this.btnUltimo.Size = new System.Drawing.Size(75, 23);
+            this.btnUltimo.Size = new System.Drawing.Size(112, 35);
             this.btnUltimo.TabIndex = 6;
             this.btnUltimo.Text = ">>";
             this.btnUltimo.UseVisualStyleBackColor = true;
             // 
             // btnNovo
             // 
-            this.btnNovo.Location = new System.Drawing.Point(15, 18);
+            this.btnNovo.Location = new System.Drawing.Point(22, 28);
+            this.btnNovo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnNovo.Name = "btnNovo";
-            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.Size = new System.Drawing.Size(112, 35);
             this.btnNovo.TabIndex = 7;
             this.btnNovo.Text = "&Novo";
             this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(96, 18);
+            this.btnSalvar.Location = new System.Drawing.Point(144, 28);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSalvar.Name = "btnSalvar";
-            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.Size = new System.Drawing.Size(112, 35);
             this.btnSalvar.TabIndex = 8;
             this.btnSalvar.Text = "&Salvar";
             this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
             // btnExcluir
             // 
-            this.btnExcluir.Location = new System.Drawing.Point(177, 18);
+            this.btnExcluir.Location = new System.Drawing.Point(266, 28);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnExcluir.Name = "btnExcluir";
-            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.Size = new System.Drawing.Size(112, 35);
             this.btnExcluir.TabIndex = 9;
             this.btnExcluir.Text = "E&xcluir";
             this.btnExcluir.UseVisualStyleBackColor = true;
             // 
             // btnAlterar
             // 
-            this.btnAlterar.Location = new System.Drawing.Point(258, 18);
+            this.btnAlterar.Location = new System.Drawing.Point(387, 28);
+            this.btnAlterar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAlterar.Name = "btnAlterar";
-            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.Size = new System.Drawing.Size(112, 35);
             this.btnAlterar.TabIndex = 10;
             this.btnAlterar.Text = "&Alterar";
             this.btnAlterar.UseVisualStyleBackColor = true;
             // 
             // frmProdutoras
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(623, 197);
+            this.ClientSize = new System.Drawing.Size(934, 303);
             this.Controls.Add(this.btnAlterar);
             this.Controls.Add(this.btnExcluir);
             this.Controls.Add(this.btnUltimo);
@@ -218,11 +238,13 @@ namespace prjLocadora
             this.Controls.Add(this.btnNovo);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmProdutoras";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Produtoras";
+            this.Load += new System.EventHandler(this.frmProdutoras_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
--- prjLocadora/frmProdutoras.cs
+++ prjLocadora/frmProdutoras.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,67 @@ namespace prjLocadora
 {
     public partial class frmProdutoras : Form
     {
+        String connectionString = @"Server=darnassus\motorhead;Database=db_230650; User Id=230650;Password=Guigoloko081092!!!;";
+        bool novo;
         public frmProdutoras()
         {
             InitializeComponent();
+        }
+
+        private void frmProdutoras_Load(object sender, EventArgs e)
+        {
+            btnSalvar.Enabled = false;
+            txtCodProd.Enabled = false;
+            txtEmailProd.Enabled = false;
+            txtProd.Enabled = false;
+            txtTelProd.Enabled = false;
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            btnNovo.Enabled = false;
+            btnSalvar.Enabled = true;
+            txtProd.Enabled = true;
+            txtEmailProd.Enabled = true;
+            txtTelProd.Enabled = true;
+            btnExcluir.Enabled = false;
+            btnPrimeiro.Enabled = false;
+            btnAnterior.Enabled = false;
+            btnProximo.Enabled = false;
+            btnUltimo.Enabled = false;
+            btnAlterar.Enabled = false;
+            novo = true;
+            txtProd.Focus();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (novo)
+            {
+                string sql = "INSERT INTO tblProdutora (nomeProd, telPro, emailProd) "
+                    + "VALUES ('"+txtProd.Text+"', '"+txtTelProd.Text+"', '"+txtEmailProd.Text+"')";
+                //MessageBox.Show(sql);
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.CommandType = CommandType.Text;
+                con.Open();
+                try
+                {
+                    int i = cmd.ExecuteNonQuery();
+                    if(i>0)
+                    {
+                        MessageBox.Show("Produtora cadastrada com sucesso");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Erro: " + ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
         }
     }
 }
